f4989b72412b29bcdb85d9d0fae52ae91b579744
GitHubFirstStep <1031404314@qq.com>
2020-12-29 20:47:04 +0800

    又加了一个按钮

--- QW/Form1.cs
+++ QW/Form1.cs
@@ -22,5 +22,10 @@ namespace QW
             Q qq = new Q();//测试同步2
             qq.Show();//获取7
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = "3";
+        }
     }
 }
