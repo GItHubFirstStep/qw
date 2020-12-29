50260b55fed7afb171a3fb5fd7b022594c74b4c9
GitHubFirstStep <1031404314@qq.com>
2020-12-29 19:49:37 +0800

    变更本地库

--- QW/Form1.cs
+++ QW/Form1.cs
@@ -20,7 +20,7 @@ namespace QW
         private void button1_Click(object sender, EventArgs e)
         {
             Q qq = new Q();//测试同步2
-            qq.Show();//是否拉取
+            qq.Show();//变更本地库
         }
     }
 }
