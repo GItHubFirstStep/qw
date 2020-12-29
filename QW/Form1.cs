9ed0df95a0d0a33eff2b45eaa6d3d4c48f109131
GitHubFirstStep <1031404314@qq.com>
2020-12-29 20:37:55 +0800

    77

--- QW/Form1.cs
+++ QW/Form1.cs
@@ -19,7 +19,7 @@ namespace QW
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Q qq = new Q();//测试同步2
+            Q qq = new Q();//77
             qq.Show();//获取6
         }
     }
